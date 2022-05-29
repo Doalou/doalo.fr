0dd0ae27218d38e632b3be9e45204ddd83cec45d
Slawk <yt.slawk.contact@gmail.com>
2022-05-29 15:11:03 +0200

    Waiting page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--gradient-color-stops))',
+      }
+    },
+  },
+  plugins: [],
+}
