282e3f1bf2238fcb2bd4e47b27ac4f1b2314ae4b
Slawk <yt.slawk.contact@gmail.com>
2022-05-30 09:18:23 +0200

    tkt

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   content: ["./**.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--gradient-color-stops))',
-      }
     },
   },
   plugins: [],
